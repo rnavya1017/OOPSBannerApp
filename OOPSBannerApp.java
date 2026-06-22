70ed3180160f7da1e50dbe35cef70c70f5091396
R Navya <ravagunta.navya@gmail.com>
2026-06-22 17:09:09 +0530

    [Navya]:Implemented UC2 to print oops pattern

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,15 @@
-package com.bridgelabz;
 
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+
+            System.out.println("   ***   " + " "+ "   ***   " + " "+ "******   " + " "+ "   ***** ");
+            System.out.println(" **   ** " + " "+ " **   ** " + " "+ "**    ** " + " "+ " **      ");
+            System.out.println("**     **" + " "+ "**     **" + " "+ "**     **" + " "+ "**       ");
+            System.out.println("**     **" + " "+ "**     **" + " "+ "**    ** " + " "+ "  *****  ");
+            System.out.println("**     **" + " "+ "**     **" + " "+ "******   " + " "+ "      ** ");
+            System.out.println(" **   ** " + " "+ " **   ** " + " "+ "**       " + " "+ " **   ** ");
+            System.out.println("   ***   " + " "+ "   ***   " + " "+ "**       " + " "+ "  *****  ");
+        }
+
     }
-}
